6152fbd9da31923142ba7856fcb8f490191c44f8
David Magomedov <vitodimaga@gmail.com>
2025-06-07 18:34:27 +0500

    Масссивы часть 2. Выполнил.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,51 @@
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("Задача 1");
+
+        int[] pay = new int[5];
+        pay[0] = 12000;
+        pay[1] = 11000;
+        pay[2] = 13000;
+        pay[3] = 15000;
+        pay[4] = 14000;
+        int sum = 0;
+        for (int i = 0; i < pay.length; i++) {
+            sum += pay[i];
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей.");
+
+        System.out.println("Задача 2");
+
+        int PayForWeek = -1;
+        for (int i = 0; i < pay.length; i++) {
+            final int current = pay[i];
+            if (current > PayForWeek) {
+                PayForWeek = current;
+            }
+        }
+        System.out.println("Максимальная сумма трат в неделю составила " + PayForWeek + " рублей");
+        for (int i = 0; i < pay.length; i++) {
+            final int current = pay[i];
+            if (current < PayForWeek) {
+                PayForWeek = current;
+            }
+        }
+        System.out.println("Минимальная сумма трат в неделю составила " + PayForWeek + " рублей");
+
+        System.out.println("Задача 3");
+
+        int averagePay = - 1;
+        for (int i = 0; i < pay.length; i++) {
+            averagePay = sum / pay.length;
+        }
+        System.out.println("Средняя сумма трат за месяц составила " + averagePay + " рублей");
+
+        System.out.println("Задача 4");
+
+        char[] reserveFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reserveFullName.length - 1; i >= 0; i--) {
+            System.out.print(reserveFullName[i]);
+        }
     }
 }
